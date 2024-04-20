d1cfdec5ab3374832a2a33d114f6ab4fa4686844
Deadlynight0 <byrayael@gmail.com>
2024-04-20 18:07:20 +0300

    feat(project:PreorityQueue) fixed AvengersPriorityQueue

--- PreorityQueue.py
+++ PreorityQueue.py
@@ -1,9 +1,17 @@
 class AvengersPriorityQueue:
-    pass
+    def __init__(self):
+        self.__count = 0
+        self.__head = None
+        self.__tail = None
 
     class Node:
-        pass
+        def __init__(self,data,prev):
+            self.data = data
+            self.prev = prev
 
-    class AvengersMission:
-        pass
+class AvengersMission:
+    def __init__(self, description, preority = 3):
+        self.__description = description
+        self.__preority = preority
+
 
